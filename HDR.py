4960c76ef9f6f20a6d8c9d8498cf2febcb78864e
phantomvoyager4 <mateusz.pilawski@gmail.com>
2025-12-01 14:03:39 +0100

    feat: basic netwrok architecture implemented

--- HDR.py
+++ HDR.py
@@ -1,14 +1,15 @@
 import numpy as np
 import idx2numpy
 
+
 def data_handling(pathimages, pathlabels):
     images = idx2numpy.convert_from_file(pathimages).reshape((60000, 784))
     images = images / 255.0
     labels = idx2numpy.convert_from_file(pathlabels)
-    map = np.identity(10)
-    labels_encoded = map[labels]
-    print(images.shape, labels_encoded.shape) #(60000, 784) (60000, 10)
-    return images, labels_encoded
+    # map = np.identity(10)
+    # labels_encoded = map[labels] 
+    print(images.shape, labels.shape) #(60000, 784) (60000, 10)
+    return images, labels
 
 
 
@@ -31,3 +32,18 @@ class Activation:
     def forward(self, input):
         self.output= np.maximum(0, input)
         return self.output
+
+class Softmax:
+    def forward (self, input):
+        exp = np.exp(input - np.max(input, axis=1, keepdims=True))
+        sum = np.sum(exp, axis=1, keepdims=True)
+        self.output = exp/sum
+        return self.output
+    
+class Loss_CategoricalCrossentropy:
+    def forward(self, y_pred, y_true):
+        y_pred = np.clip(y_pred, 0.0000001, 0.99999999)
+        predicted = y_pred[range(y_pred.shape[0]), y_true]
+        loss = -np.log(predicted)
+        mean_loss = np.mean(loss)
+        return mean_loss
