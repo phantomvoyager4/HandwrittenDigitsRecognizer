bbacbde52e067c92a2d6ebbb7a6b3864791d81d4
phantom737 <96076197+phantomvoyager4@users.noreply.github.com>
2025-12-01 16:17:41 +0100

    feat: prepared chain rule which creates neural network with 2 hidden layers

--- HDR.py
+++ HDR.py
@@ -6,18 +6,8 @@ def data_handling(pathimages, pathlabels):
     images = idx2numpy.convert_from_file(pathimages).reshape((60000, 784))
     images = images / 255.0
     labels = idx2numpy.convert_from_file(pathlabels)
-    # map = np.identity(10)
-    # labels_encoded = map[labels] 
-    print(images.shape, labels.shape) #(60000, 784) (60000, 10)
     return images, labels
 
-
-
-pathimagess = 'dataset/train-images.idx3-ubyte'
-pathlabelss = 'dataset/train-labels.idx1-ubyte'
-data_handling(pathimagess, pathlabelss)
-
-#NEURONS LOGIC
 class Layer:
     def __init__(self, n_inputs, n_neurons):
         self.weights = np.random.randn(n_inputs, n_neurons) * 0.01
@@ -27,7 +17,6 @@ class Layer:
         self.output = np.dot(input, self.weights) + self.biases  
         return self.output
 
-#ACTIVATE NEURONS USING RELU
 class Activation:
     def forward(self, input):
         self.output= np.maximum(0, input)
@@ -47,3 +36,27 @@ class Loss_CategoricalCrossentropy:
         loss = -np.log(predicted)
         mean_loss = np.mean(loss)
         return mean_loss
+
+
+
+pathimagess = 'dataset/train-images.idx3-ubyte'
+pathlabelss = 'dataset/train-labels.idx1-ubyte'
+images, labels = data_handling(pathimagess, pathlabelss)
+
+hidden_layer1 = Layer(n_inputs=784, n_neurons=128)
+activation = Activation()
+hidden_layer2 = Layer(n_inputs=128, n_neurons=64)
+output_layer = Layer(n_inputs=64, n_neurons=10)
+output_layer_by_probability = Softmax()
+losscalc = Loss_CategoricalCrossentropy()
+
+
+# Layer1 -> ReLU -> Layer2 -> ReLU -> OutputLayer -> Softmax
+first_layer = hidden_layer1.fpropagation(input=images)
+first_activate = activation.forward(input=first_layer)
+second_layer = hidden_layer2.fpropagation(input=first_activate)
+second_activation = activation.forward(input=second_layer)
+output_layer_output = output_layer.fpropagation(input=second_activation)
+results =  output_layer_by_probability.forward(input=output_layer_output)
+loss = losscalc.forward(y_pred=results, y_true=labels)
+print("Loss:", loss) # ~-ln(1/10)
